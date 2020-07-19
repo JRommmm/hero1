014cc3f9ae90735426fb4be12c9ba369b7270908
JRommmm <juanromanutti@Juans-MacBook-Pro.local>
2020-07-19 13:56:42 -0400

    hero1 Front end 1st deployment

---
+++ build/precache-manifest.2da8b0070bea5fa4fcebae37035e1b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d729695a96b059800a9388da240cede5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba81e9972e5cfa3441d6",
+    "url": "/static/js/2.cc710347.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc710347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea63cf4de09ee8063684",
+    "url": "/static/js/main.ba9009de.chunk.js"
+  },
+  {
+    "revision": "3a7bf3c6e5674494ca12",
+    "url": "/static/js/runtime-main.395da0f9.js"
+  }
+]);
